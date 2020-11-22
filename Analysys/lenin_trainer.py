45c11f83f68e6040dea58e8bf2378bca295f5673
sevakabrits <seva.kabrits@gmail.com>
2020-11-22 16:03:05 +0300

    bugs in nn

--- Analysys/lenin_trainer.py
+++ Analysys/lenin_trainer.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 import pprint
+BS = 4
 
 
 class NumDs(utils_data.Dataset):
@@ -23,80 +24,98 @@ class NumDs(utils_data.Dataset):
 
 class CommunistNN:
 
-    def __init__(self, input_data, outputdata):
+    def __init__(self, input_data, outputdata, mode="train"):
 
         trainset = NumDs(input_data[:-len(input_data)//4], outputdata[:-len(input_data)//4])
-        trainloader = utils_data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=2)
+        trainloader = utils_data.DataLoader(trainset, batch_size=BS, shuffle=True, num_workers=2)
 
         testset = NumDs(input_data[-len(input_data)//4:], outputdata[-len(input_data)//4:])
-        testloader = utils_data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
+        testloader = utils_data.DataLoader(testset, batch_size=BS, shuffle=False, num_workers=2)
 
-        pnn = Stalin3000_anal_probe(5)
-        loss_fn = torch.nn.CrossEntropyLoss()
+        self.pnn = Stalin3000_anal_probe(5)
+        if mode == "test" or mode == "next  train":
+            self.pnn.load_state_dict(torch.load('pnn_weights'))
+            if mode == "test":
+                self.pnn.eval()
 
-        # выбираем алгоритм оптимизации и learning_rate
-        learning_rate = 1e-4
-        optimizer = torch.optim.Adam(pnn.parameters(), lr=learning_rate)
-        losses = []
+        if  mode != "test":
+            loss_fn = torch.nn.MSELoss()
 
-        # итерируемся
-        for epoch in notebook.tqdm(range(20)):
-            pnn.train()
-            running_loss = 0.0
-            for i, batch in enumerate(notebook.tqdm(trainloader)):
-                # так получаем текущий батч
-                X_batch, y_batch = batch
+            # выбираем алгоритм оптимизации и learning_rate
+            learning_rate = 1e-3
+            optimizer = torch.optim.Adam(self.pnn.parameters(), lr=learning_rate,weight_decay=5e-4)
+            # pprint.pprint(list(self.pnn.parameters()))
+            losses = []
 
-                # обнуляем веса
-                optimizer.zero_grad()
+            # итерируемся
+            for epoch in notebook.tqdm(range(50)):
+                self.pnn.train()
+                running_loss = 0.0
+                for i, batch in enumerate(notebook.tqdm(trainloader)):
+                    # так получаем текущий батч
+                    X_batch, y_batch = batch
 
-                # forward + backward + optimize
-                y_pred = pnn(X_batch.view(5,1))
-                # print(y_batch, "\n\n", y_pred)
-                loss = loss_fn(y_pred, y_batch)
-                loss.backward()
-                optimizer.step()
+                    # обнуляем веса
+                    optimizer.zero_grad()
 
-                # выведем текущий loss
-                running_loss += loss.item()
-                # выведем качество каждые 2000 батчей
-                if i % 200 == 199:
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    losses.append(running_loss)
-                    running_loss = 0.0
+                    # forward + backward + optimize
+                    y_pred = self.pnn(X_batch)
+                    # print(y_batch.shape, "\n\n", y_pred.shape)
+                    loss = loss_fn(y_pred, y_batch)
+                    loss.backward()
+                    optimizer.step()
 
-            plt.figure(figsize=(10, 7))
-            plt.plot(np.arange(len(losses)), losses)
-            plt.show()
+                    # выведем текущий loss
+                    running_loss += loss.item()
+                    # выведем качество каждые 2000 батчей
+                    if i % 20 == 19:
+                        print('[%d, %5d] loss: %.3f' %
+                              (epoch + 1, i + 1, running_loss / 2000))
+                        losses.append(running_loss)
+                        running_loss = 0.0
 
-        print('Обучение закончено')
+                if epoch%40 == 39:
+                    plt.figure(figsize=(10, 7))
+                    plt.plot(np.arange(len(losses)), losses)
+                    plt.show()
 
-        error = 0
-        total = 0
-        pnn.eval()
+            print('Обучение закончено')
+
+            error = 0
+            total = 0
+            self.pnn.eval()
+            with torch.no_grad():
+                for data in testloader:
+                    images, labels = data
+                    y_pred = self.pnn(images)
+                    predicted = y_pred
+                    print(y_pred, "\n\n", y_batch, "\n\n\n\n")
+                    c = sum([abs(predicted[i]-labels[i]) for i in range(len(labels))])/len(labels)
+                    print("\n",c,"\n")
+                    for i in range(BS):
+                        label = labels[i]
+                        error += c
+                        total += 1
+
+            print('Error is : %f' % (c/total))
+
+            torch.save(self.pnn.state_dict(), "./pnn_weights")
+
+    def eval_single(self, x):
         with torch.no_grad():
-            for data in testloader:
-                images, labels = data
-                y_pred = pnn(images)
-                _, predicted = torch.max(y_pred, 1)
-
-                c = sum([abs(predicted[i]-labels[i]) for i in range(len(labels))])/len(labels)
-                for i in range(1):
-                    label = labels[i]
-                    error += c
-                    total += 1
-
-        for i in range(10):
-            print('Error is : %f %%' % (c/total))
-
-        torch.save(pnn.state_dict(), "./pnn_weights")
+            self.pnn.eval()
+            res = self.pnn(torch.Tensor(x).float().unsqueeze(0))
+        return res
 
 if __name__ ==  "__main__":
     mat = list()
     with open('test_data.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            mat.append([float(row['Temperature (K)']),float(row['Luminosity(L/Lo)']),float(row['Radius(R/Ro)']),float(row['Star type']),0,float(row['Absolute magnitude(Mv)'])])
+            mat.append([float(row['Temperature (K)']),float(row['Luminosity(L/Lo)']),float(row['Radius(R/Ro)']),float(row['Star type']),1,float(row['Absolute magnitude(Mv)'])])
     # pprint.pprint(np.array(mat)[:,-1:])
-    CommunistNN(np.array(mat,dtype="float32")[:,:-1],np.array(mat,dtype="float32")[:,-1:])
+    # mat = list(zip(list(range(1,1000)),list(range(4000,3001,-1)),list(range(10,10000,10)),list(range(1001,2000)),list(map(lambda x: 0,range(1,1000))),list(map(lambda x: x[0]*x[1]*x[2]*x[3],zip(list(range(1,1000)),list(range(4000,3001,-1)),list(range(10,10000,10)),list(range(1001,2000)),list(map(lambda x: 0,range(1,1000))))))))
+    # pprint.pprint(mat)
+    Stalin_with_probe = CommunistNN(np.array(mat,dtype="float32")[:,:-1],np.array(mat,dtype="float32")[:,-1:], mode="train")
+    pprint.pprint(Stalin_with_probe.eval_single([3068, 0.0024, 0.17, 0, 1]))
+    # pprint.pprint(list(Stalin_with_probe.pnn.parameters()))
--- Analysys/pnn.py
+++ Analysys/pnn.py
@@ -7,24 +7,19 @@ class Stalin3000_anal_probe(nn.Module):
         # вызов конструктора предка
         super(Stalin3000_anal_probe, self).__init__()
         self.n = n
-        self.insider = nn.Linear(n, 4*n)
-        self.hl1 = nn.Linear(4*n, 2*n)
-        self.hl2 = nn.Linear(2*n, 4*n)
-        self.hl3 = nn.Linear(4*n, int(1.5*n))
-        self.hl4 = nn.Linear(int(1.5*n), n)
-        self.outsider = nn.Linear(n, 1)
-        self.dropout = nn.Dropout()
-        self.bnf2 = nn.BatchNorm1d(num_features=4*n, eps=1e-07, momentum=0.1, affine=True, track_running_stats=True)
-        self.bnf1 = nn.BatchNorm1d(num_features=2*n, eps=1e-07, momentum=0.1, affine=True, track_running_stats=True)
-        self.bnf3 = nn.BatchNorm1d(num_features=int(1.5*n), eps=1e-07, momentum=0.1, affine=True, track_running_stats=True)
+        self.insider = nn.Linear(n, n+2)
+        self.hl1 = nn.Linear(n+2, n+2)
+        self.hl2 = nn.Linear(n+2, int(n/2))
+        self.outsider = nn.Linear(int(n/2), 1)
+        # self.bnf2 = nn.BatchNorm1d(num_features=4*n, eps=1e-07, momentum=0.1, affine=True, track_running_stats=True)
+        # self.bnf1 = nn.BatchNorm1d(num_features=2*n, eps=1e-07, momentum=0.1, affine=True, track_running_stats=True)
+        # self.bnf3 = nn.BatchNorm1d(num_features=int(1.5*n), eps=1e-07, momentum=0.1, affine=True, track_running_stats=True)
 
     def forward(self, x):
-        for _ in range(self.n // 3):
-            x = F.dropout(F.relu(self.bnf2(self.insider(x))), p=0.3)
-            for _ in range(self.n//2):
-                x = F.dropout(F.relu(self.bnf1(self.hl1(x))), p=0.5)
-                x = F.dropout(F.relu(self.bnf2(self.hl2(x))), p=0.5)
-            x = F.dropout(F.relu(self.bf3(self.hl3(x))), p=0.3)
-            x = F.dropout(F.relu(self.hl4(x)), p=0.3)
-        x = F.dropout(F.relu(self.outsider(x)), p=0.2)
+        # for _ in range(self.n // 3):
+        x = F.dropout(F.relu(self.insider(x)), p=0.5)
+        # for _ in range(self.n//2):
+        x = F.dropout(F.relu(self.hl1(x)), p=0.5)
+        x = F.dropout(F.relu(self.hl2(x)), p=0.5)
+        x = F.dropout(F.relu(self.outsider(x)), p=0.5)
         return x
